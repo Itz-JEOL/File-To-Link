1b330c443eba6e476f04b28f0268899a54fbbad3
Jeol Paul <123471314+Itz-JEOL@users.noreply.github.com>
2023-04-15 20:57:01 +0530

    Update stream_routes.py

--- Adarsh/server/stream_routes.py
+++ Adarsh/server/stream_routes.py
@@ -23,6 +23,21 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(_):
+    return web.json_response(
+        {
+            "server_status": "running",
+            "uptime": get_readable_time(time.time() - StartTime),
+            "telegram_bot": "@" + StreamBot.username,
+            "connected_bots": len(multi_clients),
+            "loads": dict(
+                ("bot" + str(c + 1), l)
+                for c, (_, l) in enumerate(
+                    sorted(work_loads.items(), key=lambda x: x[1], reverse=True)
+                )
+            ),
+            "version": __version__,
+        }
+    )
     
 
 
